0a0b0545de068ce3d8c68ad788c94f4930f47e6f
yiren <yiren.goo@gmail.com>
2020-11-12 16:29:24 +0800

    form editcontext

--- BlazorWasm/Program.cs
+++ BlazorWasm/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Fluxor;
+using BlazorStrap;
 
 namespace BlazorWasm
 {
@@ -17,7 +18,7 @@ namespace BlazorWasm
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-
+            builder.Services.AddBootstrapCss();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddFluxor(config=>{
                 config.ScanAssemblies(typeof(Program).Assembly)
